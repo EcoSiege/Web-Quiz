465b32272fb3a13f12acd153abdff231e74d98dd
EcoSiege <153530802+EcoSiege@users.noreply.github.com>
2023-12-31 23:24:33 -0600

    psuedo code

---
+++ assets/JS/script.js
@@ -0,0 +1,9 @@
+//start button
+//quiz display
+    //change after the  quiz is started and after each question is answered 
+    //display options for the user to pick
+    //if the right answer is picked the score goes up
+    //if the wrong answer is piacke the timer goes down
+//timer
+//save scores
+//let user put ther inintials with the score after their round
