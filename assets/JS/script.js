721f9367e4ccc6768d3cc5ea1e5854a5f5e0df37
EcoSiege <153530802+EcoSiege@users.noreply.github.com>
2024-01-01 21:04:43 -0600

    added variables to the javascript and added an event listener.

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -1,9 +1,31 @@
+var question = document.querySelector("#theme-switcher");
+var answer1 = document.querySelector("#1");
+var answer2 = document.querySelector("#2");
+var answer3 = document.querySelector("#3");
+var answer4 = document.querySelector("#4");
+var start = document.querySelector('#start');
+var timeLeft = 60;
+
 //start button
+start.addEventListener('click', function(){
 //quiz display
     //change after the  quiz is started and after each question is answered 
     //display options for the user to pick
     //if the right answer is picked the score goes up
     //if the wrong answer is piacke the timer goes down
+
+},
 //timer
+function timer(){
+    var timerInterval = setInterval(function() {
+        timeLeft--;
+
+        if(secondsLeft <= 0) {
+            clearInterval(timerInterval);
+          }
+}, 1000);
+});
+
 //save scores with initials
+//make an array for the questions that can be looped through to display each question and its answers
     
